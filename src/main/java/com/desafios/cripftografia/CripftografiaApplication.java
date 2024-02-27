510c34ef4e0aa0c20e12ea8cc4b0b5f0875643be
Kauan-Code19 <pkauprofissional@gmail.com>
2024-02-26 21:23:25 -0300

    projeto criado

---
+++ src/main/java/com/desafios/cripftografia/CripftografiaApplication.java
@@ -0,0 +1,13 @@
+package com.desafios.cripftografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CripftografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CripftografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafios/cripftografia/CripftografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafios.cripftografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CripftografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
